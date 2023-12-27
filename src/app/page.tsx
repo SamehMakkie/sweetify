4cb84a6ffd94b2c9b65d446f78dcbfc5c105eddb
Sameh Makkie <smhmkkh@hotmail.co.uk>
2023-12-28 00:03:55 +0200

    Add Hero Section (Image)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/views/Hero/Hero";
 import Navbar from "@/views/Navbar/Navbar";
 
 export default function Home() {
   return (
     <section className="flex flex-col w-full min-h-screen items-center">
       <Navbar />
+      <Hero />
     </section>
   );
 }
---
+++ src/views/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <img src="/Hero.png" className='w-full h-auto bg-cover' />
+  )
+}
+
+export default Hero

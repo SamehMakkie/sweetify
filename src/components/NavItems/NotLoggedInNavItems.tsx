ed03503fe7816cad943a889b6382773b99967e61
Sameh Makkie <smhmkkh@hotmail.co.uk>
2024-02-03 22:20:17 +0200

    Fix login and register button responsivness issue in the navbar on smartphones

--- src/components/NavItems/NotLoggedInNavItems.tsx
+++ src/components/NavItems/NotLoggedInNavItems.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import React from "react";
+import IconBtn from "../IconBtn/IconBtn";
+import { CiUser } from "react-icons/ci";
 
 const NotLoggedInNavItems = () => {
   return (
     <>
+    <div className="hidden sm:block">
       <Link href={"/login"}>
         <button className="btn btn-ghost rounded-full text-white hover:bg-lighter-dark-secondary">
           Log in
@@ -14,6 +17,13 @@ const NotLoggedInNavItems = () => {
           Register
         </button>
       </Link>
+    </div>
+    <div className="block sm:hidden">
+      <Link href={"/login"}>
+        <IconBtn Icon={CiUser} />
+      </Link>
+      
+      </div>
     </>
   );
 };

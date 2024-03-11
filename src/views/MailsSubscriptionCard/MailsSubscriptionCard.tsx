9fab5cb1cf7c68ba77e51fb3faa28e9eddb1c6c8
Sameh Makkie <smhmkkh@hotmail.co.uk>
2024-03-12 01:53:49 +0200

    Fix MailSubscriptionCard on safari devices with smartphone width

--- src/views/MailsSubscriptionCard/MailsSubscriptionCard.tsx
+++ src/views/MailsSubscriptionCard/MailsSubscriptionCard.tsx
@@ -38,14 +38,16 @@ const MailsSubscriptionCard = () => {
   };
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 bg-dark-secondary drop-shadow-2xl rounded-2xl">
-      <img
-        src="/mails-subscription.png"
-        className="w-full h-full object-cover rounded-t-2xl md:rounded-tr-none md:rounded-l-2xl"
-      />
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 bg-dark-secondary drop-shadow-2xl rounded-2xl overflow-hidden">
+      <div className="w-full col-span-1 h-full">
+        <img
+          src="/mails-subscription.png"
+          className="w-full h-full object-cover rounded-t-2xl md:rounded-tr-none md:rounded-l-2xl"
+        />
+      </div>
       <form
         noValidate
-        className="flex flex-col justify-center gap-5 p-10 sm:p-32 md:p-20 lg:p-28 xl:p-32"
+        className="flex flex-col col-span-1 justify-center gap-5 p-10 sm:p-32 md:p-20 lg:p-28 xl:p-32"
         onSubmit={handleSubmit(handleMailListSignup)}>
         <h2 className="text-3xl sm:text-4xl text-primary">
           Subscribe to our newsletter
